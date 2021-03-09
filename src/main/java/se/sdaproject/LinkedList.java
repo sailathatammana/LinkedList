1e62d1b17de8a74ede179f2c11b2a94082ff4b77
sailathatammana <sailathatammana1997@gmail.com>
2021-03-09 13:51:52 +0100

    Added searcg fet size methods

--- src/main/java/se/sdaproject/LinkedList.java
+++ src/main/java/se/sdaproject/LinkedList.java
@@ -31,7 +31,7 @@ public class LinkedList {
             // this means, first != null
 
             // step 2: find the last node
-            
+
             // Key: introduce a temporary variable!
             // we can then update this temporary variable,
             // without changing "first"!
@@ -53,6 +53,85 @@ public class LinkedList {
             current.next = node;
         }
 
+    }
+    // Searching a element in data structure
+    public int search(int num){
+        if (first== null){
+            return -1;
+        }
+        else{
+            Node current= first;
+            int index=0;
+            if (current.elem==num){
+                return index;
+            }
+            else {
+                while(current.next!=null){
+                    index+=1;
+                    if (current.next.elem== num) {
+                        return index;
+                    }
+                    current=current.next;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public Node get(int index) {
+        if (first == null) {
+            return null;
+        }
+        try {
+            Node current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current;
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    public int size(){
+        try {
+            Node current = first;
+            int size = 1;
+            while (current.next != null) {
+                current = current.next;
+                size++;
+            }
+            return size;
+        } catch (NullPointerException ignored) {
+            return 0;
+        }
+
+    }
+
+    public boolean remove (int num) {
+        try {
+            if (search(num) < 0) {
+                return false;
+            } else if (search(num)== 0 && size() >1) {
+                first = get(search(num)+1);
+                return true;
+            } else if (search(num) == 0 && size() == 1){
+                first = null;
+                return true;
+            }
+            else {
+                Node previousNode = get(search(num)-1);
+                if (get(search(num)+1) == null ) {
+                    previousNode.next = null;
+                } else {
+                    previousNode.next = get(search(num)+1);}
+
+                return true;
+
+            }
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 
     // useful for testing!
@@ -76,7 +155,7 @@ public class LinkedList {
             // go through the chain of nodes, starting with
             // "first"
 
-            Node current = first;            
+            Node current = first;
             // treat first element specially:
             builder.append("" + current.elem);
 
--- src/test/java/se/sdaproject/LinkedListTests.java
+++ src/test/java/se/sdaproject/LinkedListTests.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class LinkedListTests {
 
     @Test
@@ -24,5 +28,131 @@ class LinkedListTests {
         LinkedList list = new LinkedList();
         String expected = "LinkedList()";
         assertEquals(list.toString(), expected);
+    }
+
+    @Test
+    void searchElementFromList(){
+        LinkedList list = new LinkedList();
+        list.add(8);
+        list.add(7);
+        Integer index = list.search(7);
+        assertEquals(1,index);
+    }
+
+    @Test
+    void search_EmptyList() {
+        LinkedList list = new LinkedList();
+
+        int expected = -1;
+        assertEquals(list.search(7), expected);
+    }
+
+    @Test
+    void search_NotFound() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        int expected = -1;
+        assertEquals(list.search(12), expected);
+    }
+
+    @Test
+    void get_FullList() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        int expected = 10;
+        assertEquals(list.get(2).elem, expected);
+    }
+
+    @Test
+    void get_EmptyList() {
+        LinkedList list = new LinkedList();
+
+        assertEquals(list.get(2), null);
+    }
+
+    @Test
+    void size_FullList() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        int expected = 3;
+        assertEquals(list.size(), expected);
+    }
+
+    @Test
+    void size_EmptyList() {
+        LinkedList list = new LinkedList();
+
+        int expected = 0;
+        assertEquals(list.size(), expected);
+    }
+
+    @Test
+    void remove_ListStartElement() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        String expected = "LinkedList(7,10)";
+        boolean remove = list.remove(5);
+        assertEquals(expected, list.toString());
+        assertTrue(remove);
+    }
+
+    @Test
+    void remove_ListEndElement() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        String expected = "LinkedList(5,7)";
+        boolean remove = list.remove(10);
+        assertEquals(expected, list.toString());
+        assertTrue(remove);
+    }
+    @Test
+    void remove_ListMiddleElement() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        String expected = "LinkedList(5,10)";
+        boolean remove = list.remove(7);
+        assertEquals(expected, list.toString());
+        assertTrue(remove);
+    }
+
+    @Test
+    void remove_ListMissingElement() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+
+        String expected = "LinkedList(5,7,10)";
+        boolean remove = list.remove(12);
+        assertEquals(expected, list.toString());
+        assertFalse(remove);
+    }
+
+    @Test
+    void remove_EmptyList() {
+        LinkedList list = new LinkedList();
+
+        String expected = "LinkedList()";
+        boolean remove = list.remove(5);
+        assertEquals(expected, list.toString());
+        assertFalse(remove);
     }
 }
